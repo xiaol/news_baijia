b52062fbcfe06f2c54ac598d8f0f334424754563
jianghao <676974774@qq.com>
2015-03-26 17:39:45 +0800

    fix

--- task/weibo_run.py
+++ task/weibo_run.py
@@ -6,6 +6,9 @@ from pymongo.read_preferences import ReadPreference
 import json
 import sys
 from requests.exceptions import ConnectionError
+import requests
+import re
+
 
 
 arg = sys.path[0].split('/')
@@ -36,6 +39,8 @@ def weiboTaskRun():
     # un_runned_docs = conn["news_ver2"]["Task"].find({"weiboOk": 0})
 
 
+    success_num = 0
+    fail_num = 0
     for doc in un_runned_docs:
         # url = doc["url"]
         url = doc["sourceUrl"]
@@ -65,7 +70,13 @@ def weiboTaskRun():
 
             conn["news_ver2"]["Task"].update({"url": url}, {"$set": {"weiboOk": 1}})
 
-        print "weiboTaskRun success, the doc url is:" + url
+            success_num += 1
+            print "weiboTaskRun success, the doc url is:" + url, "sucess num:", success_num
+
+            continue
+
+        fail_num += 1
+        print "weiboTaskRun fail, the doc url is:" + url, "fail num:", fail_num
 
 
 
@@ -95,12 +106,24 @@ def GetOneWeibo(title):
 def nerTaskRun():
 
     un_runned_docs = conn["news_ver2"]["Task"].find({"nerOk": 0})
+    index = 0
     for doc in un_runned_docs:
         url = doc["url"]
         content = get_content_by_url(url)
 
+        #获取内容 有问题，跳过
+        if not content:
+            continue
+
         content_after_cut = jieba.cut(content)
+
+        content_after_cut = "".join(content_after_cut)
+
         ne = getNe(content_after_cut)
+
+        #ner 有问题，跳过
+        if not ne:
+            continue
 
         try:
             conn["news_ver2"]["googleNewsItem"].update({"sourceUrl": url}, {"$set": {"ne": ne}})
@@ -108,21 +131,112 @@ def nerTaskRun():
             print "ner fail, the doc url is:", url
             continue
 
-        conn["news_ver2"]["Task"].update({"url": url}, {"$set": {"nerOk": 1}})
-        print "ner success, the doc url is:", url
+        conn["news_ver2"]["Task"].update({"url": url}, {"$set": {"nerOk": 1, "contentOk": 1}})
+        index += 1
+        print "ner success, the doc url is:", url, "num:", index
 
 
 
 
 def get_content_by_url(url):
 
-    content = "坐飞机真危险"
-    return content
+    apiUrl_img = "http://121.41.75.213:8080/extractors_mvc_war/api/getImg?url="
+    apiUrl_text = "http://121.41.75.213:8080/extractors_mvc_war/api/getText?url="
+
+    apiUrl_img += url
+    apiUrl_text += url
+
+    r_img = requests.get(apiUrl_img)
+    r_text = requests.get(apiUrl_text)
+
+    img = (r_img.json())["imgs"]
+    text = (r_text.json())["text"]
+    try:
+        conn["news_ver2"]["googleNewsItem"].update({"sourceUrl": url}, {"$set": {"imgUrls": img, "content": text}})
+    except Exception as e:
+        print ">>>>>>>>save content error", e, "the url is :", url
+        return None
+
+    return text
+
 
 def getNe(content_after_cut):
 
-    ne = ["飞机", "危险"]
+    print "content_after_cut", content_after_cut
+    apiUrl = "http://121.41.75.213:8080/ner_mvc/api/ner?sentence=" + content_after_cut
+
+    r = requests.get(apiUrl)
+    print '>>>>>>>>>',r.text, "encoding", r.encoding
+    json_r = json.loads(r.text)
+    if json_r["error_code"] != 0:
+        return None
+
+    ne = parseNerResult(json_r)
+
     return ne
+
+
+def parseNerResult(json_r):
+
+    time_filter = ["今天","明天","后天"]
+    times = []
+    locs = []
+    persons = []
+    gpes = []
+    orgs = []
+    pat = re.compile('<[^<>]+?>')
+    for t in json_r["misc"]:
+
+        t = re.sub(pat, '', t)
+        if t in time_filter or len(t) <= 2 or not isTime(t):
+            continue
+        times.append(t)
+
+    for loc in json_r["loc"]:
+
+        loc = re.sub(pat, '', loc)
+        locs.append(loc)
+
+    for person in json_r["person"]:
+
+        person = re.sub(pat, '', person)
+        persons.append(person)
+
+    for gpe in json_r["gpe"]:
+
+        gpe = re.sub(pat, '', gpe)
+        gpes.append(gpe)
+
+    for org in json_r["org"]:
+
+        org = re.sub(pat, '', org)
+        orgs.append(org)
+
+
+    ne = {"time": times, "gpe": gpes, "org": orgs, "person": persons, "loc": locs}
+
+    return ne
+
+def isTime(string):
+
+    num = 0
+    if '年' in string:
+        num += 1
+
+    if '月' in string:
+        num += 1
+
+    if '日' in string:
+        num += 1
+
+    if num >= 2:
+        return True
+
+    return False
+
+
+
+
 
 
 #摘要抽取任务，对每条新闻进行摘要抽取候，存入mongo
@@ -130,11 +244,16 @@ def abstractTaskRun():
 
     un_runned_docs = conn["news_ver2"]["Task"].find({"abstractOk": 0})
 
+    success_num = 0
     for doc in un_runned_docs:
         url = doc["url"]
         content = get_content_by_url(url)
 
+        if not content:
+            continue
+
         abstract_here = KeywordExtraction.abstract(content)
+        print ">>>>>>>>abstract:", abstract_here
 
         try:
             conn["news_ver2"]["googleNewsItem"].update({"sourceUrl": url}, {"$set": {"abstract": abstract_here}})
@@ -144,7 +263,8 @@ def abstractTaskRun():
 
         conn["news_ver2"]["Task"].update({"url": url}, {"$set": {"abstractOk": 1}})
 
-        print "abstract update success, the doc url is: ", url
+        success_num += 1
+        print "abstract update success, the doc url is: ", url, "success num:", success_num
 
 
 # 正文，和图片获取 task
@@ -202,37 +322,44 @@ def isOnlineTaskRun():
 # task 从googleNewsItem 表中取没上线新闻到 Task表
 def newsToTaskRun():
 
-    offline_docs = conn["news_ver2"]["googleNewsItem"].find({"$or": [{"isOnline": 0}, {"$exists": {"isOnline": 0}}]})
+    offline_docs = conn["news_ver2"]["googleNewsItem"].find({"$or": [{"isOnline": 0}, {"isOnline": {"$exists": 0}}]})
 
+    index = 0
     for doc in offline_docs:
+        index += 1
         url = doc["sourceUrl"]
         title = doc["title"]
 
         conn["news_ver2"]["Task"].update({"url": url}, {"$set": {"url": url, "title": title, "weiboOk": 0, "zhihuOk": 0,
-                                                                 "abstractOk": 0, "contentOk": 0, "nerOk": 0, "isOnline": 0}})
-        print "title"
+                                                                 "abstractOk": 0, "contentOk": 0, "nerOk": 0, "isOnline": 0}}, upsert=True)
+        print "title", title, "num:", index
     print "newsToTaskRun complete"
 
 def mainRun():
 
     while True:
+        try:
+            weiboTaskRun()
 
-        weiboTaskRun()
+            nerTaskRun()
 
-        nerTaskRun()
+            abstractTaskRun()
 
-        abstractTaskRun()
+            zhihuTaskRun()
 
-        cont_pic_titleTaskRun()
+            isOnlineTaskRun()
 
-        zhihuTaskRun()
-
-        isOnlineTaskRun()
+        except:
+            continue
 
 if __name__ == '__main__':
     # weiboTaskRun()
 
-   newsToTaskRun()
+    #newsToTaskRun()
+
+    # nerTaskRun()
+
+    abstractTaskRun()
 
 
 
