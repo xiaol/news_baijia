3cbc10776bb35c40d719ed7c73f7db2d8ebe77d5
Ivan liu <liu.xiao.in@gmail.com>
2015-05-23 16:33:11 +0800

    fixed bugs for channel list order,maybe revert it later for android version updated

--- controller/channel_get.py
+++ controller/channel_get.py
@@ -2,6 +2,7 @@
 
 from config import dbConn
 import re
+import pymongo
 
 DBStore = dbConn.GetDateStore()
 channelEnum = {'时事': 0, '娱乐': 1, '科技': 2, '国际': 3, '体育': 4, '财经': 5, '港台':6, '社会':7}
@@ -11,11 +12,11 @@ channelDict = {0: ['内地', '社会', '国内'], 1: ['娱乐'], 2: ['科技'], 
 def fetch_channel(channelId, page=1, limit=50):
     conn = DBStore._connect_news
     channelTags = [re.compile(x) for x in channelDict[channelId]]
-    docs = conn['news_ver2']['googleNewsItem'].find({"isOnline": 1, "sourceSiteName": {"$in": channelTags}}).sort([("createTime", -1)]).skip(
-        (page - 1) * limit).limit(limit)
+    docs = conn['news_ver2']['googleNewsItem'].find({"isOnline": 1, "sourceSiteName": {"$in": channelTags}}).sort("createTime", pymongo.DESCENDING).limit(50)
     results_docs = []
     for doc in docs:
         results_docs.append(doc)
+        print doc
 
     return results_docs
 
