d8aa3e21a445f63e8a8846baaeb440ad6c1115df
Ivan liu <liu.xiao.in@gmail.com>
2015-06-08 16:15:22 +0800

    fixed one paragraph updated

--- controller/content_get.py
+++ controller/content_get.py
@@ -152,20 +152,20 @@ def project_comments_to_paragraph(doc, comments):
         textblocks.append({'content':content})
     if len(textblocks) == 1:
        for comments_elem in comments:
-        dict_len = len(comments_elem)
-        comment_result = comments_elem[str(dict_len)]
-        userName = comment_result['author_name'].replace('网易','')
-        if 'author_img_url' in comment_result:
-            userIcon = comment_result['author_img_url']
-        else:
-            userIcon = ""
+           dict_len = len(comments_elem)
+           comment_result = comments_elem[str(dict_len)]
+           userName = comment_result['author_name'].replace('网易', '')
+           if 'author_img_url' in comment_result:
+               userIcon = comment_result['author_img_url']
+           else:
+               userIcon = ""
 
-        point = {'sourceUrl': doc['sourceUrl'], 'srcText': comment_result["message"], 'desText': "",
-                 'paragraphIndex': 0, 'type': "text_paragraph", 'uuid': "", 'userIcon': userIcon ,
-                 'userName': userName, 'createTime': comment_result["created_at"],
-                 "up": comment_result["up"], "down": comment_result["down"], "comments_count":len(comments)}
-        points.append(point)
-        return points
+           point = {'sourceUrl': doc['sourceUrl'], 'srcText': comment_result["message"], 'desText': "",
+                    'paragraphIndex': 0, 'type': "text_paragraph", 'uuid': "", 'userIcon': userIcon,
+                    'userName': userName, 'createTime': comment_result["created_at"],
+                    "up": comment_result["up"], "down": comment_result["down"], "comments_count": len(comments)}
+           points.append(point)
+       return points
 
     textblock_dict = {}
     paragraphIndex = 0
