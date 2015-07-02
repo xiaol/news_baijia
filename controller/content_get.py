5a9645982790e58233d3ee2a0f0383f5f68ceb94
YuanHuiAce <eva1-1@163.com>
2015-07-02 17:05:26 +0800

    point

--- controller/content_get.py
+++ controller/content_get.py
@@ -4,13 +4,14 @@ from PIL import Image
 from config import dbConn
 import requests
 from home_get import del_dup_relatedoc
-
+import datetime
 import jieba
 import gensim
 from sklearn.svm import SVC
 from math import sqrt
 import numpy as np
 import math
+from weibo.Comments import guid
 
 DBStore = dbConn.GetDateStore()
 
@@ -379,6 +380,28 @@ def getText(doc):
             for k, item_doc in _doc.iteritems():
                 if "txt" in item_doc.keys():
                    return item_doc['txt']
+
+def AddPoint(sourceUrl, srcText, desText, paragraphIndex, type, uuid, userIcon, userName, userId, platformType, srcTextTime): #type title abstract content
+    conn = DBStore._connect_news
+    point = {}
+    point['sourceUrl'] = sourceUrl
+    point['srcText'] = srcText
+    point['desText'] = desText
+    point['paragraphIndex'] = paragraphIndex
+    point['type'] = type
+    point['uuid'] = uuid
+    point['userIcon'] = userIcon
+    point['userName'] = userName
+    now = datetime.datetime.now()
+    point['createTime'] = now
+    point['userId'] = userId
+    point['platformType'] = platformType
+    point['srcTextTime'] = srcTextTime
+    point['commentId'] = guid('news_baijia')
+    conn['news_ver2']['pointItem'].insert(point)
+
+    result = {'response': 200}
+    return result
 
 def project_comments_to_paragraph(doc, comments):
     points = []
