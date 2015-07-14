52325e80bcf226a1bcedddb626264de9c35c8dce
YuanHuiAce <eva1-1@163.com>
2015-07-14 11:35:47 +0800

    loadmorecontent

--- controller/channel_get.py
+++ controller/channel_get.py
@@ -5,10 +5,12 @@ import re
 import pymongo
 import operator
 import json
+
 DBStore = dbConn.GetDateStore()
 channelEnum = {'时事': 0, '娱乐': 1, '科技': 2, '国际': 3, '体育': 4, '财经': 5, '港台': 6, '社会': 7}
 channelDict = {0: ['内地', '社会', '国内'], 1: ['娱乐'], 2: ['科技'], 3: ['国际'], 4: ['体育'], 5: ['财经'], 6: ['港台'], 7: ['社会']}
 special_source = ["观察", "网易"]
+
 
 def fetch_channel(channelId, page=1, limit=50):
     conn = DBStore._connect_news
@@ -26,7 +28,9 @@ def fetch_channel(channelId, page=1, limit=50):
 def newsFetch_channel(channelId, page=1, limit=50):
     conn = DBStore._connect_news
     if channelId >= 0:
-        docs = conn['news_ver2']['NewsItems'].find({"channel_id": str(channelId), "imgnum": {'$gt': 0}, 'create_time':{ '$exists': True}}).sort("create_time", pymongo.DESCENDING).skip(
+        docs = conn['news_ver2']['NewsItems'].find(
+            {"channel_id": str(channelId), "imgnum": {'$gt': 0}, 'create_time': {'$exists': True}}).sort("create_time",
+                                                                                                         pymongo.DESCENDING).skip(
             (page - 1) * limit).limit(limit)
     else:
         channel_doc = conn['news_ver2']['ChannelItems'].find_one({"channel_id": str(channelId)})
@@ -34,16 +38,44 @@ def newsFetch_channel(channelId, page=1, limit=50):
         docs = conn['news_ver2']['googleNewsItem'].find(
             {"isOnline": 1, "sourceSiteName": {'$regex': channel_name}}).sort(
             "createTime", pymongo.DESCENDING).skip((page - 1) * limit).limit(limit)
+    results_docs = reorganize_news(docs, conn)
+    return results_docs
+
+
+def getImg(doc):
+    for _doc in doc['content']:
+        for k, item_doc in _doc.iteritems():
+            if "img" in item_doc.keys():
+                return item_doc['img']
+
+
+def reorganize_news(docs, conn):
     results_docs = []
-    docs = reorganize_news(docs)
+    eventId_dict = {}
+
     for doc in docs:
+        if 'eventId' in doc.keys():
+            if doc['eventId'] in eventId_dict.keys():
+                eventId_dict[doc['eventId']].append(doc)
+            else:
+                eventId_dict[doc['eventId']] = [doc]
+        else:
+            results_docs.append(doc)
+
+    for (eventId, eventList) in eventId_dict.items():
+        doc = constructEvent(eventList)
+        if doc:
+            results_docs.append(doc)
+
+    results_docs = sorted(results_docs, key=operator.itemgetter("create_time"), reverse=True)
+    for doc in results_docs:
         isWeiboFlag = 0
         isBaikeFlag = 0
         isZhihuFlag = 0
         isImgWallFlag = 0
         isCommentsFlag = 0
         if "_id" in doc.keys():
-            doc['id']= str(doc['_id'])
+            doc['id'] = str(doc['_id'])
             del doc['_id']
         if "text" in doc.keys():
             doc.pop('text')
@@ -61,7 +93,7 @@ def newsFetch_channel(channelId, page=1, limit=50):
             url = doc['source_url']
             doc['sourceUrl'] = doc['source_url']
         if "content" in doc.keys():
-            doc['imgUrl']=getImg(doc)
+            doc['imgUrl'] = getImg(doc)
         if "content" in doc.keys():
             doc.pop('content')
         if "douban" in doc.keys():
@@ -78,11 +110,10 @@ def newsFetch_channel(channelId, page=1, limit=50):
             if doc["imgWall"]:
                 isImgWallFlag = 1
             del doc["imgWall"]
-        sublist = []
 
-        if "sublist" in doc.keys():
-            sublist = doc["sublist"]
-            del doc["sublist"]
+        # if "sublist" in doc.keys():
+        #     sublist = doc["sublist"]
+        #     del doc["sublist"]
 
         if "weibo" in doc.keys():
             weibo = doc["weibo"]
@@ -111,63 +142,56 @@ def newsFetch_channel(channelId, page=1, limit=50):
                 isZhihuFlag = 1
             del doc["zhihu"]
 
-        doc_comment = conn["news_ver2"]["commentItems"].find_one({"relateUrl": url})
-        if doc_comment:
-            if doc_comment["comments"]:
+        doc_crawl_comment = conn["news_ver2"]["commentItems"].find_one({"relateUrl": url})
+        doc_point_comment = conn["news_ver2"]["pointItem"].find_one({"sourceUrl": url})
+        if doc_crawl_comment:
+            if doc_crawl_comment["comments"]:
                 isCommentsFlag = 1
 
+        if doc_point_comment:
+            if doc_point_comment["srcText"]:
+                isCommentsFlag = 1
         doc["isWeiboFlag"] = isWeiboFlag
         doc["isBaikeFlag"] = isBaikeFlag
         doc["isZhihuFlag"] = isZhihuFlag
         doc["isImgWallFlag"] = isImgWallFlag
         doc["isCommentsFlag"] = isCommentsFlag
 
-        results_docs.append(doc)
-        print doc
-
     return results_docs
 
-def getImg(doc):
-    for _doc in doc['content']:
-        for k, item_doc in _doc.iteritems():
-            if "img" in item_doc.keys():
-                return item_doc['img']
 
-def reorganize_news(docs):
-    results_docs = []
-    eventId_dict = {}
-
-    for doc in docs:
-        if 'eventId' in doc.keys():
-            if doc['eventId'] in eventId_dict.keys():
-                eventId_dict[doc['eventId']].append(doc)
-            else:
-                eventId_dict[doc['eventId']] = [doc]
-        else:
-            results_docs.append(doc)
-    print results_docs
-    print eventId_dict
-    for doc in results_docs:
-        doc['sublist']=(constructEvent(eventId_dict))
-
-    results_docs= sorted(results_docs,key=operator.itemgetter("create_time"), reverse=True)
-    return results_docs
-
-def constructEvent(eventId_dict):
-    for (eventId, eventList) in eventId_dict.items():
+def constructEvent(eventList):
         result_doc = {}
         sublist = []
+        flag = 0
         for eventElement in eventList:
             if eventElement['eventId'] == eventElement["source_url"]:
                 result_doc = eventElement
+                flag = 1
             else:
-                subElement={'sourceSitename': eventElement['start_title'], 'url': eventElement['source_url'], 'title': eventElement['title'],'create_time':eventElement['create_time']}
+                subElement = {'sourceSitename': eventElement['start_title'], 'url': eventElement['source_url'],
+                              'title': eventElement['title'], 'create_time': eventElement['create_time']}
                 sublist.append(subElement)
+            result_doc["create_time"] = eventElement['create_time']
             result_doc["sublist"] = sublist
-            return sublist
+        if flag ==1:
+            return result_doc
+
+
+def loadMoreFetchContent(channelId, type, time, limit,id):
+    conn = DBStore._connect_news
+    if type == 0:
+        docs = conn['news_ver2']['NewsItems'].find(
+            {"channel_id": str(channelId), "imgnum": {'$gt': 0}, 'create_time': {'$exists': True},'create_time':{'$gte': time},'_id' : {'$nin' :[id]}}).sort("create_time",pymongo.ASCENDING).limit(limit)
+    else:
+
+        docs = conn['news_ver2']['NewsItems'].find(
+            {"channel_id": str(channelId), "imgnum": {'$gt': 0}, 'create_time': {'$exists': True},'create_time':{'$lte': time},'_id' : {'$nin' :[id]}}).sort("create_time",pymongo.ASCENDING).limit(limit)
+    results_docs = reorganize_news(docs, conn)
+    return results_docs
+
 
 def GetRelateNews(relate, filter_url):
-
     # if not relate:
     #     return
 
@@ -182,7 +206,6 @@ def GetRelateNews(relate, filter_url):
     distinct_response_urls = []
     sumList = []
 
-
     total_relate = [left_relate, mid_relate, bottom_relate, opinion, deep_relate]
 
     for relate in total_relate:
@@ -190,7 +213,7 @@ def GetRelateNews(relate, filter_url):
             if not e["title"]:
                 continue
             if e["sourceSitename"] not in sourceNameSet and e["url"] not in filter_url:
-                e["user"]=""
+                e["user"] = ""
                 distinctList.append(e)
                 distinct_response_urls.append(e["url"])
                 sourceNameSet.add(e["sourceSitename"])
