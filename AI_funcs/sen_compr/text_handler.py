6f12d6e99d8fcdebb98fd903920525c34110c4cb
Gavin <Gavin@WeiliangGuosMac.lan>
2015-07-28 20:30:33 +0800

    SentenceCompressor class added.

--- AI_funcs/sen_compr/text_handler.py
+++ AI_funcs/sen_compr/text_handler.py
@@ -12,82 +12,55 @@ import requests
 import re
 
 
-
-"""
-from HTMLParser import HTMLParser
-
-
-class MLStripper(HTMLParser):
-    def __init__(self):
-        self.reset()
-        self.fed = []
-
-    def handle_data(self, d):
-        self.fed.append(d)
-
-    def get_data(self):
-        return ''.join(self.fed)
-
-
-class TextPreprocessor:
-    def __init__(self, url_link='http://localhost:8080/sentcompr/'):
-        self.url_link = url_link
-
-    def get_html_content(self, s):
-        sentence_seg = s
-        result = requests.get(self.url_link+sentence_seg)
-        result = result.text
-        result = TextPreprocessor.strip_tags(result)
-        #Remove all HTML tags.
-        # result = re.sub("<.*?>", "", result)
-        print(result)
-        return result
-
+class SentenceCompressor:
+    def __init__(self, raw_sentence="", api_url="http://60.28.29.37:8080/SentenceCompressor?sentence="):
+        self.api_url = api_url
+        self.raw_sentence = raw_sentence
 
     # If you want to match a certain part of a string using a regex,
     # just add a new regex-string condition pair into this method body.
-    @staticmethod
-    def normalize_orders(matchobj):
+    def normalize_orders(self, matchobj):
         if matchobj.group() == "%":
             return "百分号"
-        elif matchobj.group() == '[':
+        elif matchobj.group() == '\[':
             return '('
         elif matchobj.group() == ']':
             return ')'
         elif matchobj.group() == "-":
+            # return "_"
             return "_"
         elif matchobj.group() == '·':
             return '_'
-        elif matchobj.group() == " ":
-            return ","
-        elif matchobj.group() == r"[,|，].*称[,|，]|“|”| |‘|’|《|》|据.*[报道|电| |消息][,|，|:| ]|在.*[前|后][,|，]":
+        # elif matchobj.group() == " ":
+        #     return ","
+        elif matchobj.group() == r"[,|，].*称[,|，]|“|”| |‘|’|《|》":
             return ""
 
+    #Collect all the regular expressions to be used.  | stands for bitwise OR.
+    def regex_collect(self):
+        rg = r"[,|，].*称[,|，]|“|”| |‘|’|《|》|%|\[|]|-|·"
+        return rg
+
     # text is the string to be pre-processed, regex is the regular expression(s).
-    def preprocess_text(self, text, regex=r"[,|，].*称[,|，]|“|”| |‘|’|《|》|%|[|]|-|·|据.*[报道|电| ][,|，|:| |消息]|在.*[前|后][,|，]"):
-        pre_processed_txt = re.sub(regex, self.normalize_orders, text)
+    def text_preprocess(self):
+        regex = self.regex_collect()
+        text = self.raw_sentence
+        pre_processed_txt = re.sub(regex, normalize_orders, text)
         return pre_processed_txt
 
-    def find_gists_of_google_news(self, skip_n=300, limit_n=5):
-        dbstore = dbConn.GetDateStore()
-        conn = dbstore._connect_news
-        result_doc = conn["news_ver2"]["googleNewsItem"].find({"isOnline":1}).skip(skip_n).limit(limit_n)
+    #Get last text segment of a sentence as last comma encountered
+    def get_last_sen_seg(self, sen=''):
+        # last_sen_seg = sen.split('，')[-1]
+        last_sen_seg = re.split(",|，", sen)[-1]
+        return last_sen_seg
 
-        gists = []
-        urls = []
-        ggs = []
-        for result_doc_elem in result_doc:
-
-            gist_raw = result_doc_elem["abstract"]
-            gist = gist_raw.encode('utf-8')
-            gg = self.preprocess_text(gist)
-            gists.append(gist)
-            ggs.append(gg)
-
-            url = result_doc_elem["sourceUrl"]
-            urls.append(url)
-        return zip(gists, ggs, urls)
-"""
+    def get_compression_result(self):
+        refined_text = self.text_preprocess()
+        sentence_ready_to_compress = get_last_sen_seg(refined_text)
+        if len(refined_text) <= 12:
+            return refined_text
+        compr_result = requests.get(self.api_url + sentence_ready_to_compress)
+        return compr_result
 
 
 
@@ -201,15 +174,21 @@ def get_quote_text(txt_str=''):
 
 
 if __name__ == '__main__':
+    a_sample_sentence_to_compress = '凌德权认为,通过中越双方对阮富仲访华的一系列精心安排可以看出,两国领导人都十分珍视中越传统友谊,并有强烈的共同意愿,推动两国全面战略合作伙伴关系长期稳定健康发展'
+    sencom = SentenceCompressor(raw_sentence=a_sample_sentence_to_compress)
+    sencom = sencom.get_compression_result()
+    print(sencom)
 
+
+
+"""
     xx = find_gists_of_google_news()
     for title, gst, gg, u , aticle in xx:
         print(aticle)
         print("\n")
         print("\n")
-        get_quote_text(aticle)
+        # get_quote_text(aticle)
         print("                ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
-        """
         gloss = get_longest_sen_seg(gst)
         glass = get_last_sen_seg(gst)
         print("\n")
@@ -229,5 +208,4 @@ if __name__ == '__main__':
         print("                Last sentence segment:    " + glass)
         html_content = get_html_content(glass)
         print("                ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■")
-        """
-
+"""
