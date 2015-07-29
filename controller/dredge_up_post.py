06761fe1e8586397ac7aa08b73918766bd936090
YuanHuiAce <eva1-1@163.com>
2015-07-29 10:35:52 +0800

    dredgeUp

--- controller/dredge_up_post.py
+++ controller/dredge_up_post.py
@@ -1,12 +1,6 @@
 # coding=utf-8
 
 from config import dbConn
-import json
-import datetime, time
-import operator
-import pymongo
-from pymongo.read_preferences import ReadPreference
-from weibo.Comments import guid
 import redis
 DBStore = dbConn.GetDateStore()
 pool = redis.ConnectionPool(host='h213', port=6379)
@@ -18,8 +12,6 @@ def dredgeUpStatus(keys):
     dict = {}
     for key in keys:
         dict[key]=r.hmget(key,"status","insertId")
-        # dict.append(r.hmget(key,"status","insertId"))
-        print r.hmget(key,"status","insertId")
     results_docs =dict
     print results_docs
     return results_docs
