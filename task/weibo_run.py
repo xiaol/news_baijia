c91d7a2aeec31bbc300f7622c4a9408f9e17c4d6
yangjw <yangjw@fal.com>
2015-11-17 10:27:04 +0800

    fix

--- task/weibo_run.py
+++ task/weibo_run.py
@@ -1114,6 +1114,11 @@ def googleNewsTaskRun():
         #     topic = extract_tags_helper(title_here)
         #     topic = 's'.join(topic)
 
+        t00 = datetime.datetime.now()
+        t00 = t00.strftime("%Y-%m-%d %H:%M:%S")
+        print "task start,%s"%t00
+        logging.warn("===============task start====================%s"%(t00))
+
         topic = title_here
         # cmd = 'scrapy crawl google.com.hk -a url=' + url_here + ' -a topic=\"'+ topic + '\"'
         cmd = '/root/workspace/news_baijia/task/script.sh ' + url_here + ' ' + topic
