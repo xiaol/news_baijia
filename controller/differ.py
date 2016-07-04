4e5d37e978fd9b6f2474adbe543f3c3156e3ed37
yangjw <yangjw@fal.com>
2016-07-04 14:47:02 +0800

    fix

--- controller/differ.py
+++ controller/differ.py
@@ -664,7 +664,7 @@ def do_relate_task(params):
         # print "%s  %s  " % (netags[i], words[i]),
       if postags[i] not in ('m', 'q', 'wp','u'):
           key_new.append(words[i])
-    print " ".join(key_new)
+    print " ".join(key_new[:3])
     params_key = {"key": " ".join(key_new)}
     data = urllib.urlencode(params_key)
     search_url ="http://120.55.88.11:8088/search?"+data
